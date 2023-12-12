479717ca991ad3af2b4123bfc586ad43ba36dd82
aramay <kasper_341@hotmail.com>
2023-12-11 19:08:10 -0800

    added Router logic

---
+++ services/Router.js
@@ -0,0 +1,63 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll("a.navlink").forEach((link) => {
+            console.log(link)
+            link.addEventListener("click", event => {
+                event.preventDefault();
+                console.log('link ckicked ');
+
+                const url = event.target.getAttribute("href")
+                Router.go(url)
+                
+            });
+        })
+        window.addEventListener("popstate", event => {
+            Router.go(event.state.route, false)
+        })
+        // Check the initial URL
+        console.log(location.pathname);
+        Router.go(location.pathname)
+    },
+    go: (route, addToHistory=true) => {
+        console.log('going to route ', route);
+
+        if (addToHistory) {
+            history.pushState({ route }, '', route)
+        }
+
+        let pageElement = null;
+
+        switch(route) {
+            case "/":
+                pageElement = document.createElement("h1")
+                pageElement.textContent = "Home Menu"
+                break;
+            case "/order":
+                pageElement = document.createElement("h1")
+                pageElement.textContent = "Orders"
+            default:
+                if (route.startsWith("/product/")) {
+                    pageElement = document.createElement("h1")
+                    pageElement.textContent = "Details"
+
+                    const paramId = route.substring(rote.lastIndexOf("-")+1);
+                    // pageElement.id = paramId;
+                    pageElement.dataset.id = paramId;
+                }
+            
+        }
+
+        if (pageElement) {
+            // document.querySelector("main").children(0).remove();
+            document.querySelector("main").innerHTML = "";
+            document.querySelector("main").appendChild(pageElement)
+
+            // scroll to default
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+        
+    }
+}
+
+export default Router;
