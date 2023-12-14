d95be2b841bdfcec3f31288e34b6099c97dd6790
aramay <kasper_341@hotmail.com>
2023-12-14 13:28:48 -0800

    Create a proxy for out store

--- services/Store.js
+++ services/Store.js
@@ -5,4 +5,21 @@ const Store = {
     cart: []
 }
 
-export default Store
+const proxiedStore = new Proxy(Store, {
+    set(target, property, value) {
+        target[property] = value
+
+        if (property === 'menu') {
+            window.dispatchEvent(new Event("appmenuchanged"));
+        }
+
+        if (property === "cart") {
+            window.dispatchEvent(new Event("appcartchanged"));
+        }
+        // important that we return true
+        // i.e. we have accepted the set
+        return true;
+    }
+})
+
+export default proxiedStore;
