2a797b38850eb339db0a963d9cff19fd21a5b5a1
aramay <kasper_341@hotmail.com>
2023-12-14 13:29:05 -0800

    Rendering the menu

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -21,6 +21,30 @@ export class MenuPage extends HTMLElement {
         const template = document.getElementById("menu-page-template")
         const content = template.content.cloneNode(true);
         this.root.appendChild(content)
+
+        window.addEventListener("appmenuchanged", () => {
+            this.render();
+        })
+    }
+
+    render() {
+        if (app.store.menu) {
+            this.root.querySelector("#menu").innerHTML = "";
+            for (let category of app.store.menu) {
+                console.log(category.name)
+                const liCategory = document.createElement('li');
+                liCategory.innerHTML = `
+                    <h3>${category.name}</h3>
+                    <ul class='category'>
+                    
+                    </ul>
+                `;
+                this.root.querySelector("#menu").appendChild(liCategory);
+            }
+        }
+        else {
+            this.root.querySelector("#menu").innerHTML = "Loading..."
+        }
     }
 }
 customElements.define("menu-page", MenuPage);
